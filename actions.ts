66e2431ed34a28bf77ad85c9cb28b4891f7a040a
Daniel Bokor <daniel.bokor@cognizant.com>
2025-02-10 17:16:34 +0200

    Implemented toggle functionality for TodoItem

--- actions.ts
+++ actions.ts
@@ -1,11 +1,13 @@
 "use server"
 
 import { Todo } from "@/types";
+import { revalidatePath } from "next/cache";
 
 const { API_URL } = process.env;
+
 export async function fetchTodos(): Promise<Todo[]> {
     await new Promise((res) => {
-        setTimeout(res, 2000);
+        setTimeout(res, 500);
     })
 
     const res = await fetch(`${API_URL}/todos`);
@@ -15,4 +17,31 @@ export async function fetchTodos(): Promise<Todo[]> {
     }
 
     return res.json();
+}
+
+export async function toggleTodo(todo: Todo): Promise<Todo> {
+    await new Promise((res) => {
+        setTimeout(res, 500);
+    })
+
+    const res = await fetch(`${API_URL}/todos/${todo.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            ...todo,
+            completed: !todo.completed,
+        })
+    });
+
+    if (!res.ok) {
+        throw new Error('Could not toggle todo.');
+    }
+
+    const newTodo = await res.json();
+    
+    revalidatePath("/");
+
+    return newTodo;
 }
--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { toggleTodo } from "@/actions";
 import { Todo } from "@/types"
 
 interface TodoItemProps {
@@ -7,9 +8,13 @@ interface TodoItemProps {
 }
 
 export function TodoItem({ todo }: TodoItemProps) {
+    const handleChange = async () => {
+        await toggleTodo(todo);
+    }
+
     return (
         <li className="flex items-center space-x-2">
-            <input id={todo.id} type="checkbox" checked={todo.completed} onChange={() => {}} />
+            <input id={todo.id} type="checkbox" checked={todo.completed} onChange={handleChange} className="cursor-pointer" />
             <label htmlFor={todo.id} className={`flex-grow cursor-pointer ${todo.completed ? "line-through text-gray-500" : ""}`}>{todo.title}</label>
         </li>
     );
